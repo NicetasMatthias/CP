c0398d1b6f4a7ce81dce9cbe943ea6d6b224b320
Nicetas Matthias <nicetas.matthias@yandex.ru>
2022-10-30 20:56:23 +0300

    Добавлен перевод

---
+++ translation_ru.ts
@@ -0,0 +1,177 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="informationDialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Окно с информацией</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="form.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>Демонстрация работы криптопротокола</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="21"/>
+        <source>Keys</source>
+        <translation>Ключи</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="36"/>
+        <source>Symm key</source>
+        <translation>Симметричный ключ</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="53"/>
+        <source>Sender</source>
+        <translation>Отправитель</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="59"/>
+        <location filename="form.ui" line="96"/>
+        <source>Open key</source>
+        <translation>Открытый ключ</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="73"/>
+        <location filename="form.ui" line="110"/>
+        <source>Private key</source>
+        <translation>Закрытый ключ</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="90"/>
+        <source>Receiver</source>
+        <translation>Получатель</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="142"/>
+        <source>Gen keys</source>
+        <translation>Сгенерировать ключи</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="167"/>
+        <source>Demostration</source>
+        <translation>Демонстрация работы</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="184"/>
+        <source>Plaint text</source>
+        <translation>Открытый текст</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="198"/>
+        <source>Enc text</source>
+        <translation>Зашифрованый текст</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="216"/>
+        <source>Decr text</source>
+        <translation>Расшифрованый текст</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="257"/>
+        <source>Original ecp</source>
+        <translation>Оригинальная ЭЦП</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="243"/>
+        <source>Sender hash</source>
+        <translation>Хеш (отправитель)</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="302"/>
+        <source>Decrypted ecp</source>
+        <translation>Результат проверки ЭЦП</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="288"/>
+        <source>Receiver hash</source>
+        <translation>Хеш (получатель)</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="342"/>
+        <source>Enc key</source>
+        <translation>Зашированный ключ</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="360"/>
+        <source>Decr key</source>
+        <translation>Расшифрованный ключ</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="397"/>
+        <source>Proced</source>
+        <translation>Работа</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="422"/>
+        <source>Information</source>
+        <translation>Информация</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="437"/>
+        <source>AES_INF</source>
+        <translation>Симметричный шифр</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="444"/>
+        <source>AES</source>
+        <translation>AES</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="460"/>
+        <source>RSA_INF</source>
+        <translation>Ассиметричный шифр</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="467"/>
+        <source>RSA</source>
+        <translation>RSA</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="483"/>
+        <source>HASH_INF</source>
+        <translation>Хеш-функция</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="490"/>
+        <source>HASH</source>
+        <translation>SHA-1</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="506"/>
+        <source>ECP_INF</source>
+        <translation>ЭЦП</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="513"/>
+        <source>ECP</source>
+        <translation>RSA</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="529"/>
+        <source>FINAL_INF</source>
+        <translation>Выводы по работе</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="536"/>
+        <source>FINAL</source>
+        <translation>Выводы</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="94"/>
+        <source>Success</source>
+        <translation>УСПЕХ</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="96"/>
+        <source>Failure</source>
+        <translation>ОШИБКА</translation>
+    </message>
+</context>
+</TS>
